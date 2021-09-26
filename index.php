1dffb77d03987e6cf4c4a01a6cfb4f774e0cb406
clr-89 <74655761+clr-89@users.noreply.github.com>
2021-09-26 17:29:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function writeSecretSentence($parametre1, $parametre2)
+{
+    
+      return $parametre1." s'incline face à ".$parametre2;
+
+}
+
+
+echo writeSecretSentence('Le lion','la lune');
+
+
+
+?>
